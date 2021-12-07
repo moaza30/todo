d49b8084b222c6ae79b8155b2cb20f086ac9e096
Moaz_Adel <moaza30@gmail.com>
2021-12-07 19:43:59 +0200

    commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,8 +6,41 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: false,
         backgroundColor: Theme.of(context).primaryColor,
-        title: Text(AppLocalizations.of(context)!.appbar_title),
+        title: Text(
+          AppLocalizations.of(context)!.appbar_title,
+          style: Theme.of(context).textTheme.headline1,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        clipBehavior: Clip.antiAlias,
+        notchMargin: 7,
+        shape: CircularNotchedRectangle(),
+        color: Theme.of(context).colorScheme.secondary,
+        elevation: 0,
+        child: BottomNavigationBar(
+          backgroundColor: Theme.of(context).colorScheme.secondary,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedIconTheme: IconThemeData(
+            color: Theme.of(context).primaryColor,
+          ),
+          unselectedIconTheme: IconThemeData(
+            color: Colors.grey,
+          ),
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: ''),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings_outlined), label: ''),
+          ],
+        ),
       ),
     );
   }
